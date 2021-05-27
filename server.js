5549bd7cf7d09fe3818f64bb75a34b8691941af9
SudarshanHV <sudarshan2807@gmail.com>
2021-05-27 22:37:21 +0530

    Boilerplate for all MERN apps complete

--- server.js
+++ server.js
@@ -1,9 +1,19 @@
 const express = require('express');
+const connectDB = require('./config/db');
+
 
 const app = express();
+//Connect Database
+connectDB();
 
 app.get('/',(req,res) => res.send('API Running'))
 
+//Define Routes
+app.use('/api/users',require('./routes/api/users.js'));
+app.use('/api/posts',require('./routes/api/posts.js'));
+app.use('/api/auth',require('./routes/api/auth.js'));
+app.use('/api/profile',require('./routes/api/profile.js'));
+
 const PORT = process.env.PORT || 5000
 
-app.listen(PORT,() => console.log('Server started on port ${PORT}'));
+app.listen(PORT,() => console.log('Server started on port',PORT));
