e481bdb89a509f56a67fd1c73fb379c59e144c43
SudarshanHV <sudarshan2807@gmail.com>
2021-05-29 09:47:41 +0530

    MiddleWare for checks + User Model Added

--- routes/api/users.js
+++ routes/api/users.js
@@ -1,10 +1,24 @@
 const express = require('express');
 const router = express.Router();
+const {check , validationResult} = require('express-validator/check');
 
-//@route:   GET api/users
-//@desc:    Test Route
+//@route:   POST api/users
+//@desc:    Register User
 //@access:  Public
 
-router.get('/',(req,res)=>res.send('Users Route'));
-
+router.post('/',[
+    check('name','Name is Required')
+    .not()
+    .isEmpty(),
+    check('email','Valid Email is Required').isEmail(),
+    check('password','Please Enter a Password with 6 characters or more')
+    .isLength({min:6})
+],
+(req,res)=> {
+    const errors = validationResult(req);
+    if(!errors.isEmpty()){
+        return res.status(400).json({errors: errors.array()});
+    }
+    res.send('Users Route');
+});
 module.exports = router;
