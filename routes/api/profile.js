d9f29dcbaa7b3a43b1560f16db2ad8fe94f1e0f2
SudarshanHV <sudarshan2807@gmail.com>
2021-05-30 12:07:16 +0530

    Fixed bugs and added GET profile routes

--- routes/api/profile.js
+++ routes/api/profile.js
@@ -82,7 +82,7 @@ router.post('/', [auth, [
         if(linkedin) profileFields.social.linkedine = linkedin;
 
         try{
-            let profile = await Profile.FindOne({user: req.user.id});
+            let profile = await Profile.findOne({user: req.user.id});
 
             if(profile){
                 //Update profile if profile exists.
@@ -104,8 +104,39 @@ router.post('/', [auth, [
             console.error(err.message);
             res.status(500).send('Server Error');
         }
-
     
+});
+
+//@route:   GET api/profile
+//@desc:    Get all user profiles
+//@access:  Public
+
+router.get('/',async (req,res)=>{
+    try {
+        const profiles = await Profile.find().populate('user',['name','avatar']);
+        res.json(profiles);
+    } catch (err) {
+        console.error(err.message);
+        res.status(500).send('Server Error');
+    }
+});
+
+//@route:   GET api/profile/user/:user_id
+//@desc:    Get all user profiles
+//@access:  Public
+
+router.get('/user/:user_id',async (req,res)=>{
+    try {
+        const profile = await Profile.findOne({user: req.params.user_id}).populate('user',['name','avatar']);
+        if(!profile) return res.status(400).json({msg: 'Profile not found'});
+        res.json(profile);
+    } catch (err) {
+        console.error(err.message);
+        if(err.kind == 'ObjectId'){
+            return res.status(400).json({msg: 'Profile not found'});
+        }
+        res.status(500).send('Server Error');
+    }
 });
 
 module.exports = router;
