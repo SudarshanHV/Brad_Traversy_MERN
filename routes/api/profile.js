ee7b3b0f285c13a9baf5fdf7b1fba008eb7f59cc
SudarshanHV <sudarshan2807@gmail.com>
2021-05-30 12:50:28 +0530

    Profile deletion, work Experience creation routes added

--- routes/api/profile.js
+++ routes/api/profile.js
@@ -139,4 +139,76 @@ router.get('/user/:user_id',async (req,res)=>{
     }
 });
 
+//@route:   DELETE api/profile
+//@desc:    Delete a specific user,profile and all posts.
+//@access:  Private
+
+router.delete('/',auth , async (req,res)=>{
+    try {
+        //Remove Profile
+        await Profile.findOneAndRemove({user: req.user.id});
+        //Remove User
+        await User.findOneAndRemove({_id: req.user.id});
+
+        res.json({msg: 'User and Profile successfully deleted'})
+    } catch (err) {
+        console.error(err.message);
+        res.status(500).send('Server Error');
+    }
+});
+
+//@route:   PUT api/profile/experience
+//@desc:    Create a new experience tab
+//@access:  Private
+
+router.put('/experience',
+    [
+    auth,
+        [
+            check('title','Title is required').not().isEmpty(),
+            check('company','Company is required').not().isEmpty(),
+            check('from','From is required').not().isEmpty()
+        ]
+    ],
+    async (req,res)=>{
+        const errors = validationResult(req);
+        if(!errors.isEmpty()){
+            res.status(400).json({errors: errors.array()});
+        }        
+
+        const {
+            title,
+            company,
+            location,
+            from,
+            to,
+            current,
+            description
+        } = req.body;
+
+        const newExp ={
+            title,
+            company,
+            location,
+            from,
+            to,
+            current,
+            description
+        }
+
+        try {
+            const profile = await Profile.findOne({user: req.user.id});
+
+            profile.experience.unshift(newExp);
+
+            await profile.save();
+            
+            res.json(profile);
+        } catch (err) {
+            console.error(err.message);
+            res.status(500).send('Server Error');
+        }
+});
+
+
 module.exports = router;
