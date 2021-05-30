d9f97fdf36d57a2a6f5f4e31c1d0afca1689bef8
SudarshanHV <sudarshan2807@gmail.com>
2021-05-30 14:45:37 +0530

    Added github repo viewer code

--- routes/api/profile.js
+++ routes/api/profile.js
@@ -308,5 +308,25 @@ router.delete('/education/:edu_id',auth,async (req,res)=>{
     } 
 });
 
-
+// @route    GET api/profile/github/:username
+// @desc     Get user repos from Github
+// @access   Public
+router.get('/github/:username', async (req, res) => {
+    try {
+      const uri = encodeURI(
+        `https://api.github.com/users/${req.params.username}/repos?per_page=5&sort=created:asc`
+      );
+      const headers = {
+        'user-agent': 'node.js',
+        Authorization: `token ${config.get('githubToken')}`
+      };
+  
+      const gitHubResponse = await axios.get(uri, { headers });
+      return res.json(gitHubResponse.data);
+    } catch (err) {
+      console.error(err.message);
+      return res.status(404).json({ msg: 'No Github profile found' });
+    }
+  });
+  
 module.exports = router;
