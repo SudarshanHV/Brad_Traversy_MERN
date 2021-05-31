97b6e14e4326a29c6f78151ed9c6d678e81502d6
SudarshanHV <sudarshan2807@gmail.com>
2021-05-31 17:55:22 +0530

    Navbar, Landing Bugs fixed

--- client/src/components/layout/Landing.js
+++ client/src/components/layout/Landing.js
@@ -2,20 +2,22 @@ import React from 'react'
 
 export const Landing = () => {
     return (
-       <section class="landing">
-        <div class="dark-overlay">
-          <div class="landing-inner">
-            <h1 class="x-large">Developer Connector</h1>
-            <p class="lead">
+       <section className="landing">
+        <div className="dark-overlay">
+          <div className="landing-inner">
+            <h1 className="x-large">Developer Connector</h1>
+            <p className="lead">
               Create a developer profile/portfolio, share posts and get help from
               other developers
             </p>
-            <div class="buttons">
-              <a href="register.html" class="btn btn-primary">Sign Up</a>
-              <a href="login.html" class="btn btn-light">Login</a>
+            <div className="buttons">
+              <a href="register.html" className="btn btn-primary">Sign Up</a>
+              <a href="login.html" className="btn btn-light">Login</a>
             </div>
           </div>
         </div>
       </section>
     )
 }
+
+export default Landing;
--- client/src/components/layout/Navbar.js
+++ client/src/components/layout/Navbar.js
@@ -14,3 +14,5 @@ export const Navbar = () => {
     </nav>
     )
 }
+
+export default Navbar;
